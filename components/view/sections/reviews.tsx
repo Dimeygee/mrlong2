6abf2f57d2d4d6c2a77a5458e028e90f9c93d233
D-meyg <adeyemioladimeji130@gmail.com>
2025-02-13 18:00:54 +0100

    reviews only

--- components/view/sections/reviews.tsx
+++ components/view/sections/reviews.tsx
@@ -8,36 +8,66 @@ import { Button } from "../../button";
 const reviews = [
   {
     id: 1,
-    name: "Linh Nguyen",
-    position: "Founder at Carrot Labs",
+    name: "Gafar Ajao",
+    position: "Full-stack Developer - RB2",
     review: [
-      "CaLan saves us a huge amount of time.",
-      "It's helping us achieve the most pixel perfect & fast websites, thanks to",
-      "full control over asset export.",
+      "Ridwan is a seasoned product designer with eyes and taste for quality designs. I'm my experience working with him, with Ridwan, no bad design, I'd recommend him to anyone in a heartbeat 🤓",
     ],
-    image: "/assets/Avatar.png",
+    image: "/assets/gafar.png",
   },
   {
     id: 2,
-    name: "John Doe",
-    position: "CEO at Tech Innovators",
+    name: "Oluyomi Olushola Michael",
+    position: "Techincal PM - Unifonic",
     review: [
-      "This tool is a game changer for web development.",
-      "The features and ease of use are unparalleled.",
-      "Highly recommend to anyone in the industry.",
+      "Ridwan is a designer who creates where others don't see it, maintaining quality user experience. He's not just an amazing colleague but a friend, teaching commitment and dedication to excellence. Honored to work with him.",
     ],
-    image: "/assets/Avatar.png",
+    image: "/assets/micheal.png",
   },
   {
     id: 3,
-    name: "Jane Smith",
-    position: "CTO at WebWorks",
+    name: "Jessica Isah",
+    position: "Product Manager - Unifonic",
     review: [
-      "Excellent service and product quality.",
-      "I have never seen such attention to detail.",
-      "CaLan's capabilities are outstanding.",
+      "Working with Ridwan is seamless. His innovation and user experience focus make him invaluable to engineering and product teams. He adds and embodies creativity. His commitment and enthusiasm will benefit any team.",
     ],
-    image: "/assets/Avatar.png",
+    image: "/assets/contact.png",
+  },
+  {
+    id: 4,
+    name: "Bakare Sofia",
+    position: "Product designer - Tribebanc",
+    review: [
+      "Ridwan is an excellent professional. He improved my design understanding and has impressive work ethic and communication skills. He adapts to any situation and stands out for his willingness to help others. I highly recommend him.",
+    ],
+    image: "/assets/contact.png",
+  },
+  {
+    id: 5,
+    name: "David ogbonna-eze",
+    position: "Lead deveolper - rechaj",
+    review: [
+      "We relied on Ridwan to bring our product vision to life, and he delivered beyond expectations. Ridwan’s user-centered approach made our app intuitive and polished. I wouldn’t hesitate to work together again!",
+    ],
+    image: "/assets/contact.png",
+  },
+  {
+    id: 6,
+    name: "managi iwu0ha",
+    position: "Product designer - Digicore",
+    review: [
+      "Working with Ridwan was a game-changer for our project. Ridwan’s designs weren’t just beautiful—they solved real problems. Every detail felt thoughtful and intentional.",
+    ],
+    image: "/assets/managi.png",
+  },
+  {
+    id: 7,
+    name: "Alvin Ahn",
+    position: "co-founder - lukuku",
+    review: [
+      "I’ve collaborated with a lot of designers, but Ridwan stands out. He has an incredible ability to listen, iterate, and transform ideas into experiences that feel seamless and impactful.",
+    ],
+    image: "/assets/contact.png",
   },
 ];
 
@@ -56,7 +86,7 @@ export const Reviews = () => {
                       className="md:w-[605px] w-[352px] min-h-[194px] shrink-0 pl-[42px] md:pl-[65px] md:min-h-[298px] justify-center rounded-[32px] flex flex-col border-[2px] border-white/[0.1] py-[20px] md:py-[58px] md:gap-[32px] gap-[20px] pr-[46px]"
                       key={index}
                     >
-                      <div className="flex gap-[32px] text-white tracking-[-0.4px] font-bold text-[12px] md:text-xl font-syne leading-[28px]">
+                      <div className="flex gap-[32px] text-white tracking-[-0.4px] font-bold text-[12px] md:text-base font-syne leading-[28px]">
                         <span>{review.review}</span>
                       </div>
                       <div className="flex gap-[32px] items-center">
@@ -70,10 +100,10 @@ export const Reviews = () => {
                         </div>
                         <div className="flex flex-col gap-[4px]">
                           <span className="font-bold uppercase text-[10px] md:text-sm text-white/[0.5] font-outfit">
-                            Linh Nguyen
+                            { review.name }
                           </span>
                           <span className="font-bold uppercase md:text-[12px] text-[8px] text-white/[0.5] font-outfit">
-                            Linh Nguyen
+                            { review.position }
                           </span>
                         </div>
                       </div>
@@ -90,7 +120,7 @@ export const Reviews = () => {
                       className="md:w-[605px] w-[352px] min-h-[194px] shrink-0 pl-[42px] md:pl-[65px] md:min-h-[298px] justify-center rounded-[32px] flex flex-col border-[2px] border-white/[0.1] py-[20px] md:py-[58px] md:gap-[32px] gap-[20px] pr-[46px]"
                       key={index}
                     >
-                      <div className="flex gap-[32px] text-white tracking-[-0.4px] font-bold text-[12px] md:text-xl font-syne leading-[28px]">
+                      <div className="flex gap-[32px] text-white tracking-[-0.4px] font-bold text-[12px] md:text-base font-syne leading-[28px]">
                         <span>{review.review}</span>
                       </div>
                       <div className="flex gap-[32px] items-center">
@@ -103,11 +133,11 @@ export const Reviews = () => {
                           />
                         </div>
                         <div className="flex flex-col gap-[4px]">
-                          <span className="md:text-[24px] font-bold uppercase  text-[10px] text-white/[0.5] font-outfit">
-                            Linh Nguyen
+                          <span className="md:text-sm font-bold uppercase  text-[10px] text-white/[0.5] font-outfit">
+                            {review.name}
                           </span>
                           <span className="font-bold uppercase md:text-[12px] text-[8px] text-white/[0.5] font-outfit">
-                            Linh Nguyen
+                            {review.position}
                           </span>
                         </div>
                       </div>
