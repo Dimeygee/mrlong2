0dcafa27eaafe48231b1d7a3b1354b75a962f1de
D-meyg <adeyemioladimeji130@gmail.com>
2026-02-16 19:42:06 +0100

    some corrections

--- components/view/container.tsx
+++ components/view/container.tsx
@@ -1,7 +1,7 @@
 
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="container mx-auto px-[20px] md:px-[117px]">{children}</div>;
+  return <div className="container mx-auto px-[20px]  xl:px-[117px]">{children}</div>;
 };
 
 export const SwiperContainer = ({ children }: { children: React.ReactNode }) => {
